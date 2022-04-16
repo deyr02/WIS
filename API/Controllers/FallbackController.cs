0734707ad89afbda3c37b6ac781162e2e01841b2
DEY RAJIB <rade002@ess.ais.ac.nz>
2022-04-16 23:48:03 +1200

    [guid]::NewGuid()

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -9,7 +9,6 @@ namespace API.Controllers
     public class FallbackController: Controller
     {
 
-
         public IActionResult Index(){
             return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
                 "wwwroot", "index.html"), "text/HTML");
