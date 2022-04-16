1819d63a16527324512a8816948cad5f7ff0d71a
DEY RAJIB <rade002@ess.ais.ac.nz>
2022-04-16 23:29:49 +1200

    simple changes

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -9,6 +9,8 @@ namespace API.Controllers
     [AllowAnonymous]
     public class FallbackController: Controller
     {
+
+        
         public IActionResult Index(){
             return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
                 "wwwroot", "index.html"), "text/HTML");
